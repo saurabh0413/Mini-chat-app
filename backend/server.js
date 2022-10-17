bb57d3d433b6bd60b7f9bce0e542f922660e7226
saurabh <saurabhfarkade83@gmail.com>
2022-10-17 20:07:24 +0530

    folders added

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("get request to homepage");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id == req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server styarted at ${PORT}`));
